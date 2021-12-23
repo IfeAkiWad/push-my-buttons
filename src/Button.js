b176afab3d765f6681f47fdbd864bc7f8ae859ee
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2021-12-23 15:17:58 -0600

    built buttons to change fonts when clicked

--- src/Button.js
+++ src/Button.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
 
 export default class Button extends Component {
+    constructor() {
+        super()
+        this.state = {
+            isClicked: ""
+        }
+    }
+
+
     render() {
         return (
             <div>
-              button comp  
+                <button onClick={() => this.setState({isClicked: "A"})} className={this.state.isClicked === "A" ? 'underline' : ""}>
+                    A: underline me
+                </button><br />
+                <button onClick={() => this.setState({isClicked: "B"})} className={this.state.isClicked === "B" ? 'italicize' : ""}>
+                    B: Italicize me
+                </button><br />
+                <button onClick={() => this.setState({isClicked: "C"})} className={this.state.isClicked === "C" ? 'bold' : ""}>
+                    C: I'm bold!
+                </button>
             </div>
         )
     }
